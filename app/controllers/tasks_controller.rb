d82de963807f991255d06d9d26ca3180a905c7c5
hiroto.takeuchi <takehiro199@icloud.com>
2019-02-08 07:13:03 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,13 +8,33 @@ class TasksController < ApplicationController
     end 
     
     def create 
+        @task = Task.new(task_params)
+        
+        if @task.save
+        flash[:success] = 'Taskが正常に投稿されました'
+        redirect_to @task 
+        
+        else
+        flash.now[:danger] = 'Taskが投稿されませんでした'
+        render :new
+        end 
+        
     end 
     
     def new 
-        @task = Tsak.new
+        @task = Task.new
     end 
     
     def update 
+        @task = Task.find(params[:id])
+            
+        if @task.update(task_params)
+            flash[:success] = 'Taskは正常に更新されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Taskは更新されませんでした'
+            render :edit 
+        end 
     end 
     
     def edit 
@@ -22,5 +42,16 @@ class TasksController < ApplicationController
     end 
     
     def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        
+        flash[:success] = 'Taskは正常に削除されました'
+        redirect_to tasks_url
+    end 
+    
+    private
+    
+    def task_params
+        params.require(:task).permit(:content)
     end 
 end
